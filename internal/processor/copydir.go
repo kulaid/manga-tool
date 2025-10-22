5d7c42ba507aa4e8f0109586a55c1de2bb59ba47
kulaid <87280154+kulaid@users.noreply.github.com>
2025-10-22 17:39:52 -0400

    s

---
+++ internal/processor/copydir.go
@@ -0,0 +1,36 @@
+package processor
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// copyDir recursively copies a directory tree, attempting to preserve permissions.
+func copyDir(src string, dst string) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+		targetPath := filepath.Join(dst, relPath)
+		if info.IsDir() {
+			return os.MkdirAll(targetPath, info.Mode())
+		}
+		srcFile, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+		dstFile, err := os.OpenFile(targetPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, info.Mode())
+		if err != nil {
+			return err
+		}
+		defer dstFile.Close()
+		_, err = io.Copy(dstFile, srcFile)
+		return err
+	})
+}
